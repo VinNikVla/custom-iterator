cfbf3c0126c6117ba19ae261c777bc8ec89eab92
VinNikVla <vinnik1905@yandex.ru>
2020-04-16 02:22:27 +0300

    добавил uniqueptr

--- main.cpp
+++ main.cpp
@@ -1,84 +1,213 @@
 #include <iostream>
 #include "template.h"	
-
+#include <queue>
+#include "MyString.h"
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	myVector<int> vec(0, 10, { 5, 1, 3, 2, 6, 8, 0, 15, -3, 3, 3, 3 ,3 });
-	std::cout << vec << "\n";
+	{
+
+		std::cout << "\n--------------------First example---------------\n";
+		myVector<int> vec(0, 10, { 5, 1, 3, 2, 6, 8, 0, 15, -3, 3, 3, 3 ,3 });
+		std::cout << vec << "\n";
+
+		vec.Add({ 1, 5, 6, 3, 5, 6, 6, 11, 6, 7 , 4 });
+
+		std::cout << vec << "\n";
+
+		vec.Delete({ 1,1,1,1, 3,5, 11, 12 });
+
+		std::cout << vec << "\n";
+		//vec.Delete({ 2,6,8,4,7 });
+		try {
+			vec.Sort("Up");
+
+		}
+		catch (const std::exception exp)
+		{
+			std::cout << exp.what() << std::endl;
+		}
+
+		std::cout << vec << "\n";
+
+		try {
+			vec.Sort("Down");
+		}
+		catch (const std::exception exp)
+		{
+			std::cout << exp.what() << std::endl;
+		}
+
+		std::cout << vec << "\n";
+
+		std::cout << "\n--------------------End of First example---------------\n";
+	}
+
+
+	{
+		std::cout << "\n--------------------Second example---------------\n";
+
+
+		myQueue<int> que;// { 1, 5, 7, 9, 3 };
+		std::cout << "Function method print\n";
+		std::cout << "Function method <<\n";
+		std::cout << que;
+
+		que.push(5);
+		que.push(4);
+		que.push(3);
+		que.push(2);
+		que.push(1);
+
+		std::cout << "range based for\n";
+
+		for (const auto& i : que)
+		{
+			std::cout << i << "\n";
+			std::cout << typeid(i).name() << "\n";
+		}
+
+		std::queue<int> qInt;
+
+		qInt.push(5);
+		qInt.push(4);
+		qInt.push(3);
+		qInt.push(2);
+		qInt.push(1);
+
+
+		std::cout << que << std::endl;
+
+		que.pop();
+		std::cout << que << std::endl;
+
+		que.pop();
+		std::cout << que << std::endl;
+
+		que.push(5);
+		std::cout << que << std::endl;
+		que.push(4);
+		std::cout << que << std::endl;
+		que.push(3);
+		std::cout << que << std::endl;
+		que.push(2);
+		std::cout << que << std::endl;
+		que.push(1);
+		std::cout << que << std::endl;
+		que.push(1);
+		std::cout << que << std::endl;
+		que.push(1);
+		std::cout << que << std::endl;
+		que.push(1);
+		std::cout << que << std::endl;
+
+		que.pop();
+		std::cout << que << std::endl;
+
+		que.pop();
+		std::cout << que << std::endl;
+
+		que.push(5);
+		std::cout << que << std::endl;
+		que.push(5);
+		std::cout << que << std::endl;
+
+		//std::cout << que;
+
+		//хз можно ли так делать
+		myQueue<int> queCopy;// { std::move(que) };
+
+		queCopy = que;
+		//queCopy{ std::move(que) };
+
+
+		std::cout << "Copy que\n";
+		std::cout << queCopy;
+
+		/*if (que == queCopy)
+		{
+			std::cout << "queues are identical!\n";
+		}
+		else
+		{
+			std::cout << "queues are not identical!\n";
+		}*/
+
+
+		myQueue<int>::iterator itb = que.begin();
+		myQueue<int>::iterator ite = que.end();
+
+		if (itb == ite)
+		{
+			std::cout << " All right!";
+		}
+
+		std::cout << *ite;
+		//std::cout << it;
+		std::cout << *itb;
+		++itb;
+		std::cout << *itb;
+		++itb;
+		std::cout << *itb;
+		++itb;
+		std::cout << *itb;
+		++itb;
+		std::cout << *itb;
+
+		std::cout << "Function method <<\n";
+		std::cout << que;
+
+		myQueue<MyString>  q1{ MyString("AAA"), MyString("qwerty"), MyString("qwerty2"), MyString("qwerty3"), MyString("qwerty4"), MyString("qwerty5"), MyString("qwerty6") };
+		//использование MyQueue в диапазонном for:
+		for (auto& el : q1) { std::cout << el << ' '; }
+		MyString s("abc");
+		q1.push(s);
+		q1.push(MyString("123"));
+		MyString s1 = q1.pop();
+		//q1.push("qqq");
+		q1.push(s);
+		std::cout << q1;
+		myQueue < MyString >  q2 = q1;
+		std::cout << q2;
+		myQueue < MyString >  q22 = std::move(q1);
+
+		myQueue<MyString> q3{ 10, MyString("!") };
+
+		std::cout << q3;
+
+		q2 = myQueue<MyString>(5, MyString("?"));
+
+		std::cout << q2;
+
+		q1 = { MyString("bbb"), MyString("ssss") };
+
+		std::cout << q1;
+
+		std::cout << q3;
+
+		q1 = q3;
+
+		std::cout << q1;
+
+		std::cout << "\n--------------------End of Second example---------------\n";
 	
-	vec.Add({ 1, 5, 6, 3, 5, 6, 6, 11, 6, 7 , 4 });
+	}
 
-	std::cout << vec << "\n";
+	{
+	std::cout << "\n--------------------Third example---------------\n";
+
+	myUniquePtr<int> ptr = new int(5);
+
+	std::cout << *ptr;
+	myUniquePtr<int> ptr1(new int(5));
+
+	MyString s("abc");
+	std::cout << s.GetString();
+	myUniquePtr<MyString> ptrStr(new MyString("abc"));
+std: cout << *ptrStr;
+	//std::cout << ptrStr->GetString();
+	std::cout << "\n--------------------End of Third example---------------\n";
+	}
 	
-	vec.Delete({ 1,1,1,1, 3,5, 11, 12 });
-	
-	std::cout << vec << "\n";
-	//vec.Delete({ 2,6,8,4,7 });
-	try{
-		vec.Sort("Up");
-
-	}
-	catch (const std::exception exp)
-	{
-		std::cout << exp.what() << std::endl;
-	}
-
-	std::cout << vec << "\n";
-
-	try {
-		vec.Sort("Down");
-	}
-	catch (const std::exception exp)
-	{
-		std::cout << exp.what() << std::endl;
-	}
-
-	std::cout << vec << "\n";
-
-	myQueue<int> que;// { 1, 5, 7, 9, 3 };
-	std::cout << "Function method print\n";
-	que.print("que");
-	std::cout << "Function method <<\n";
-	std::cout << que;
-
-	que.push(5);
-	que.push(4);
-	que.push(3);
-	que.push(2);
-	que.push(1);
-
-	std::cout << "range based for\n";
-
-	for (const auto& i : que)
-	{
-		std::cout << i << " ";
-	}
-
-
-
-	myQueue<int>::iterator itb = que.begin();
-	myQueue<int>::iterator ite = que.end();
-
-	if (itb == ite)
-	{
-		std::cout << " All right!";
-	}
-
-	std::cout << *ite;
-	//std::cout << it;
-	std::cout << *itb;
-	++itb;
-	std::cout << *itb;
-	++itb;
-	std::cout << *itb;
-	++itb;
-	std::cout << *itb;
-	++itb;
-	std::cout << *itb;
-
-	std::cout << "Function method <<\n";
-	std::cout << que;
-
 	return 0;
 }
--- template.h
+++ template.h
@@ -88,18 +88,23 @@ class myQueue
 {
 
 public:
+
 	class iterator
 	{
 	public:
-		iterator(myQueue* ptr, unsigned int index) :ptrQueue(ptr), ind(index) {};
+		//конструктор
+		iterator(const myQueue* ptr, unsigned int index) :ptrQueue(ptr), ind(index) {};
+		//оператор инкрементации
 		iterator& operator++() { ind++;ind %= ptrQueue->cap; return (*this); }
+		//оператор сравнения
 		bool operator != (const iterator& other) const { return !((ptrQueue->p == other.ptrQueue->p) && (ind == other.ind)); }
+		//оператор равенства
 		bool operator == (const iterator& other) const { return ((ptrQueue->p == other.ptrQueue->p) && (ind == other.ind));}
 		const T& operator*() const { return ptrQueue->p[ind]; }//только для чтения
-		T& operator*() {return ptrQueue->p[ind];}//для чтения и записи вроде как
+//		T& operator*() {return ptrQueue->p[ind];}//для чтения и записи вроде как
 
 	private:
-		myQueue* ptrQueue;
+		const myQueue* ptrQueue;
 		unsigned int ind;
 
 	};
@@ -109,13 +114,18 @@ private:
 	unsigned int head;//первый элемент
 	unsigned int tail;//последний элемент
 	T* p;//динамический массив
+	const unsigned int delta = 10;
+
 public:
+	//коструктор
 	myQueue() :cap(10), cnt(0), head(0), tail(0), p(new T[cap]) { }
+
+	//конструтор копирования
 	myQueue(const myQueue& other):cap(other.cnt + 10),cnt(other.cnt), head(0), tail(cnt), p(new T[cap])
 	{
 
 
-		auto itb = other.begin();
+		myQueue<T>::iterator itb = other.begin();
 			
 			for (int i = 0; i < cnt; i++)
 			{
@@ -125,31 +135,105 @@ public:
 
 
 	}
-	void push(const T& item)
+
+	myQueue(const unsigned int& nmbr, const T& data):cap(nmbr + 10), cnt(nmbr), head(0),tail(nmbr), p(new T[cap])
+	{
+		//T tmp{ std::move(data) };
+		for (int i = 0; i < nmbr;i++)
+		{
+			p[i] = data;
+		}
+	}
+
+	//конструктор копирования(move semantic)
+	myQueue(myQueue&& other)
+	{
+		p = other.p;
+		head = other.head;
+		tail = other.tail;
+		cnt = other.cnt;
+		cap = other.cap;
+		other.p = nullptr;
+	}
+
+	//итераторы
+	iterator begin() const { return iterator(this, head); }
+	iterator end() const {return iterator(this, tail);}
+
+	iterator cbegin()	const { return iterator(this, head); }
+	iterator cend()		const { return iterator(this, tail); }
+
+	/*const_iterator cbegin() { return const_iterator(this, head); }
+	const_iterator cend() { return const_iterator(this, tail); }*/
+
+	//выделение памяти
+	void newMemory(unsigned int add)
 	{
 		
-		if (cnt == cap - 1)
-		{
-			//перераспределение памяти
 
+		try {
+			cap = add + delta;
+			p = new T[cap];
 		}
+		catch(std::bad_alloc err)
+		{
+			std::cout << err.what() << std::endl;
+		}
+
+	}
+
+	//добавление в конец очереди
+	void push(const T& item)
+	{
+		//std::cout << "Добавляемые значения без зацикливания\n";
+		//for (int i = 0; i < cnt; i++)
+		//{
+		//	std::cout << "p[" << i << "]" << " = " << p[i] << std::endl;
+		//}
+		
+		if (cnt == cap - 1)
+		{ 
+			T* p2;
+			p2 = p;
+			p2[tail] = item;
+			newMemory(cap);
+			//перераспределение памяти
+			for(int i = 0; i < cnt;i++)
+			{
+				p[i] = p2[(head + i) % cnt];
+			}
+			head = 0;
+			tail = cnt;
+			delete[] p2;
+		}
+		//std::cout << "Tail before add " << tail << std::endl;
 		p[tail] = item;
 		cnt++;
 		tail = (tail + 1) % cap;
+		//std::cout << "Tail after add " << tail << std::endl;
 		
 	}
 	//move operATOR
 
-
+	//удаление первого элемента
 	T pop()
 	{
 		//ПЕРЕМЕЩАЮЩИЙ КОНСТРУКТОР
+		//не сделано, если очередь пуста
+		try{
 
-		T tmp{ std::move(p[head]) };
-		head = (head +1 ) % cap;
-		cnt--;
-		return tmp;
+			T tmp{ std::move(p[head]) };
+			head = (head + 1) % cap;
+			cnt--;
+			return tmp;
+		}
+		catch (std::bad_alloc err)
+		{
+			std::cout << err.what() << std::endl;
+		}
 	}
+
+	//конструктор с листом инициализации
 	myQueue(const std::initializer_list<T>& init):cap(init.size()*2), cnt(init.size()), head(0), tail(0), p(new T[cap])
 	{
 
@@ -160,36 +244,124 @@ public:
 		}
 	}
 
+	//деструктор
 	~myQueue()
 	{
 			delete[] p;
 	}
 
-	void print(const char* obj)
-	{
-		std::cout << "Очередь " << obj << std::endl;
-		for (int i = 0; i < cnt; i++)
-		{
-			std::cout << p[i] << " ";
-		}
-		std::cout << std::endl;
-	}
-
+	//перегруженный стандартный вывод
 	friend std::ostream& operator << (std::ostream& out, const myQueue<T>& queue)
 	{
-		out << "Capacity: " << queue.cap << " " << "Size: " << queue.cnt << std::endl;
-		for (int i = 0; i < queue.cnt; i++)
-		{
-			out << queue.p[i] << " ";
+		try {
+			out << "Capacity: " << queue.cap << " " << "Size: " << queue.cnt << std::endl
+				<< "Head: " << queue.head << " " << "Tail: " << queue.tail << std::endl;
+			for (const auto& i : queue)
+			{
+				out << i << " ";
+			}
+			out << std::endl;
+			return out;
 		}
-		out << std::endl;
-		return out;
+		catch (std::bad_alloc err)
+		{
+			std::cout << err.what() << std::endl;
+		}
 	}
 
 	//оператора присваивания класический
-	//оператор присваивания move
-	//сравнение на равенство
-	iterator begin() { return iterator(this, head); }
-	iterator end() { return iterator(this, tail); }
+	myQueue<T>& operator=(const myQueue<T>& other)
+	{
+		
+		//проверка на nullptr
+		if (other.p == nullptr)
+		{
+			delete[]p;
+			p = nullptr;
+		}
+		else if(this != &other)
+		{
+			if (other.cnt > (cap - 1))
+			{
+				delete[] p;
+				newMemory(other.cnt);
+			}
+			head = 0;
+			tail = 0;
+			cnt = 0;
+			for (const auto& i: other)
+			{
+				push(i);
+			}
+		}
 
+		return *this;
+	}
+
+	//оператор присваивания move//НЕ ДОДЕЛАН
+	myQueue<T>& operator=(myQueue<T>&& other)
+	{
+		//delete p;
+		p = other.p;
+		head = other.head;
+		tail = other.tail;
+		cnt = other.cnt;
+		cap = other.cap;
+		other.p = nullptr;
+		return *this;
+	}
+	
+	
+	//сравнение на равенство
+	bool equal(const myQueue<T>& other)
+	{
+		
+			if (cnt != other.cnt)
+			{
+				return false;
+			}
+			else
+			{
+				unsigned int j = head;
+				for (const auto& i : other)
+				{
+					if (p[j] != i)
+					{
+						return false;
+					}
+					j = (j + 1) % cap;
+				}
+			}
+			return true;
+		
+	}
+	
+
+};
+
+
+template<typename T>
+class myUniquePtr
+{
+public:
+	//конструктор
+	myUniquePtr(T* ptr)
+	{
+		this->m_ptr = ptr;
+		std::cout << "Constructor\n";
+	}
+	//деструктор
+	~myUniquePtr()
+	{
+		delete m_ptr;
+		std::cout << "Destructor\n";
+	}
+	//оператор разыменования
+	T& operator *()
+	{
+		return *m_ptr;
+	}
+
+private:
+	T* m_ptr;
 };
